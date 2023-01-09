5f5d4e32643b422869d82a6402a10a12789a44c2
unknown <souissi13mouldi@gmail.com>
2023-01-09 23:24:07 +0100

    undo undo

--- index.js
+++ index.js
@@ -2,28 +2,6 @@ const express = require("express");
 const app = express();
 const cors = require("cors");
 const connect = require("./db/connect");
-const Move = require("./models/Move");
-
-//sockets
-const http = require("http").Server(app);
-const io = require("socket.io")(http, {
-  cors: { origin: "*" },
-});
-
-const changeStream = Move.watch();
-
-changeStream.on("change", (next) => {
-  if (next.operationType === "insert") {
-    io.emit("addedMove", next.fullDocument);
-  }
-  if (next.operationType === "delete") {
-    io.emit("deletedMove", next.documentKey);
-  }
-});
-
-io.on("connection", (socket) => {
-  console.log("a user connected", socket.id);
-});
 
 // import routes
 const user = require("./routes/user");
@@ -43,7 +21,7 @@ app.use("/api/account", account);
 app.use("/api/move", move);
 
 const PORT = process.env.PORT || 5000;
-http.listen(PORT, (err) => {
+app.listen(PORT, (err) => {
   if (err) {
     console.log(err);
   } else {
