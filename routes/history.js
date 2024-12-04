b97523d7a66525c7cbed951c6d45a8ed0470b7dd
gold <souissi13mouldi@gmail.com>
2024-12-04 16:40:19 -0500

    history interval

--- routes/history.js
+++ routes/history.js
@@ -2,9 +2,22 @@ const router = require("express").Router();
 const History = require("../models/History");
 const isAuth = require("../permssions/isAuth");
 
-router.get("/", isAuth, async (req, res) => {
+router.get("/:start/:end", isAuth, async (req, res) => {
   try {
-    const history = await History.find({ shop: req.user.shop }).sort({
+    const start = req.params.start;
+    const end = req.params.end;
+
+    if (!start || !end) {
+      return res.status(400).send("invalid time interval");
+    }
+
+    const history = await History.find({
+      shop: req.user.shop,
+      date: {
+        $gte: new Date(start),
+        $lte: new Date(end),
+      },
+    }).sort({
       date: -1,
     });
     res.status(200).send(history);
