7f21e18ee8a67cbe22119d36e5b23d521ec56aef
gold <souissi13mouldi@gmail.com>
2024-12-09 17:16:00 -0500

    fix

--- routes/backup.js
+++ routes/backup.js
@@ -1,4 +1,4 @@
-const router = require("express").Router;
+const router = require("express").Router();
 const Move = require("../models/Move");
 const User = require("../models/User");
 const Shop = require("../models/Shop");
