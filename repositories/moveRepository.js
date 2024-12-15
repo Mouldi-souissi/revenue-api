1bcc7145a685266c8786d3022d18b75797af3311
gold <souissi13mouldi@gmail.com>
2024-12-14 20:49:14 -0500

    await

--- repositories/moveRepository.js
+++ repositories/moveRepository.js
@@ -2,19 +2,19 @@ const Move = require("../models/Move");
 
 class MoveRepository {
   async find(query) {
-    return await Move.find(query).sort({ date: -1 });
+    return Move.find(query).sort({ date: -1 });
   }
 
   async findById(id) {
-    return await Move.findById(id);
+    return Move.findById(id);
   }
 
   async save(move, session = null) {
-    return await move.save({ session });
+    return move.save({ session });
   }
 
   async deleteById(id, session = null) {
-    return await Move.findByIdAndRemove(id, { session });
+    return Move.findByIdAndRemove(id, { session });
   }
 }
 
